675556542c6af29b0ac754a5d8234d9b9f1ad722
sakthi <sakthivelit059@gmail.com>
2020-07-19 18:25:33 +0530

    Implement web socket

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 // Import Packages
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 const cors = require("cors");
+let interval;
 
 // Import environment config
 require("dotenv/config");
 
-// Initiate express app
-const app = express();
+// connect to remote mongodb (Mongodb Atlas)
+const MONGOOSE_URL = process.env.DB_CONNECTION;
+mongoose.connect(MONGOOSE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+// create mongo connection to track db connection status
+const mongo = mongoose.connection;
+
+//log on connection error
+mongo.on("error", (error) => {
+  console.error("mongo: " + error.name);
+});
+
+//log on successful connection
+mongo.on("connected", () => {
+  console.log("mongo: Connected");
+});
+
+//log on disconnection
+mongo.on("disconnected", () => {
+  console.warn("mongo: Disconnected");
+});
 
 // Common middleware
 app.use(cors());
@@ -24,10 +51,21 @@ app.use("/", authRoutes);
 app.use("/users", userRoutes);
 app.use("/resources", resourceRoutes);
 
-//Connect to DB
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
-  console.log("Connected to DB!");
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("room", function (data) {
+    const { id, status } = data;
+    io.sockets.emit("status", { id, status });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
 });
 
 //Port listening
-app.listen(3000);
+server.listen(8000, function () {
+  var port = server.address().port;
+  console.log(`🚀 Server is running on http://localhost:${port}`);
+});
